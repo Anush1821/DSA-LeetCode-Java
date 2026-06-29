6823281d575c89f138a84e21715387fc0f336e39
Anush1821 <anushchilaka@gmail.com>
2026-06-29 21:37:44 +0530

    sloved divide two integer

---
+++ Mathematics/DivedeTwoIntegers.java
@@ -0,0 +1,14 @@
+package Mathematics;
+
+public class DivedeTwoIntegers {
+    public int divide(int dividend, int divisor) {
+
+       if (dividend == Integer.MIN_VALUE && divisor == -1) {
+           return Integer.MAX_VALUE;
+       } 
+        int result = dividend/divisor;
+
+      return result;  
+    }
+    
+}
