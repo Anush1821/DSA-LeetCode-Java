c13225184d61c6390b234704b8da96f70baf6cb9
Anush1821 <anushchilaka@gmail.com>
2026-07-02 23:15:11 +0530

    sloved 3 lead code problems on sql database

---
+++ SQL/CustomerPlacingtheLargestNumberOfOrders.sql
@@ -0,0 +1,29 @@
+-- 586. Customer Placing the Largest Number of Orders
+-- Solved
+-- Easy
+-- Topics
+-- premium lock icon
+-- Companies
+-- Hint
+-- SQL Schema
+-- Pandas Schema
+-- Table: Orders
+
+-- +-----------------+----------+
+-- | Column Name     | Type     |
+-- +-----------------+----------+
+-- | order_number    | int      |
+-- | customer_number | int      |
+-- +-----------------+----------+
+-- order_number is the primary key (column with unique values) for this table.
+-- This table contains information about the order ID and the customer ID.
+ 
+
+-- Write a solution to find the customer_number for the customer who has placed the largest number of orders.
+
+-- The test cases are generated so that exactly one customer will have placed more orders than any other customer.
+
+select customer_number  from Orders
+group by customer_number 
+order by count(*) desc
+limit 1;
